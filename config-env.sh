e7622639ad97df5537804a00f7c4fcbd2244615f
Ariya Agustian <ariya.riyadh@wgs-id.com>
2020-11-27 21:12:53 +0700

    add config-env bash

---
+++ config-env.sh
@@ -0,0 +1,12 @@
+#! / bin / bash
+heroku config:set APP_NAME=CafePOS
+heroku config:set APP_ENV=production
+heroku config:set APP_KEY=base64:sdhj1ACNuU1JURInR+GBo+jI0zPeihJh3rUrjQkA9/U=
+heroku config:set APP_DEBUG=true
+heroku config:set APP_URL=https://cafepos.herokuapp.com
+heroku config:set DB_CONNECTION=pgsql
+heroku config:set DB_HOST=ec2-54-235-116-235.compute-1.amazonaws.com
+heroku config:set DB_PORT=5432
+heroku config:set DB_DATABASE=d86csdm5fh4ie5
+heroku config:set DB_USERNAME=azrbjpljooiszy
+heroku config:set DB_PASSWORD=d9ccabc88175d018953be3a8ba7e2df313d735c4638c87a7e73fe5c82a0e1d11
